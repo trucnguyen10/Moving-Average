9957e90312ed7edbcf84e165769dee60ea2ab133
Truc Nguyen <87359204+trucnguyen10@users.noreply.github.com>
2022-02-26 22:00:20 -0500

    Update MovingAverage.java

--- MovingAverage.java
+++ MovingAverage.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class MovingAverage {
-	long sum, windowsize, sizeN;
+	long sum, size, windowsize;
 	static boolean used = false;
 	static long minVal = 0; 
 	static long maxVal = 0;
@@ -15,16 +15,15 @@ public class MovingAverage {
 	}
 
 	//Constructor
-	public MovingAverage() {
+	public MovingAverage() { // culmulative
 		this.sum = 0;
-		this.windowsize = 0;
+		this.size = 0;
 	}
 	
-	ArrayList<Long> list;	
+	ArrayList<Long> list= new ArrayList<Long>() ;	
 	
-	public MovingAverage(long N) {
-		this.list = new ArrayList<Long>();
-		this.sizeN = N;
+	public MovingAverage(long N) { // simple
+		this.windowsize = N;
 	}
 	
 	// methods 
@@ -39,29 +38,12 @@ public class MovingAverage {
 			if(N > maxVal) maxVal = N;
 		}
 		sum+=N;
-		windowsize++;
-	}
-	public double avg()
-	{
-		if(windowsize == 0) return 0.0;
-		else return ((double) sum) / windowsize;
-	}
-	
-	
-	// method 
-	public void add(long N) {
-		if(!used) {
-			minVal = N;
-			maxVal = N;
-			used = true;
-		}
-		else 
+		size++;
+		if(windowsize <= size)
 		{
-			if (N < minVal) minVal = N;
-			if (N > maxVal) maxVal = N;
+			list.add(N);
 		}
-		list.add(N);
-	}
+}
 	public long total(long count) {
 		long sum = 0;
 		int i = list.size() - 1;
@@ -72,11 +54,21 @@ public class MovingAverage {
 		}
 		return sum;
 	}
-	public double avg() {
+	
+	public double avg()
+	{
+		if(windowsize <= size)
+		{
 		long count = list.size();
 		if(count==0) return 0.0;
-		else if(count > size) return ((double) total(size)) / size;
+		else if(count > windowsize) return ((double) total(windowsize)) / windowsize;
 		else return ((double) total(count) / count);
+		}
+		else 
+		{
+			if(size == 0) return (double) 0;
+			else return ((double) sum) / size;
+		}
 	}
 }
 	
